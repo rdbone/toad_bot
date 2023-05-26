4ac5cf8d77ed6e89a28cbf09d73afac0b5d8dfb0
rdbone <baysir17@gmail.com>
2023-05-26 11:18:06 +0300

    Доработана функциональность нахождения созвездия по планете

--- bot.py
+++ bot.py
@@ -1,26 +1,49 @@
-import logging
+import logging, settings, ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date
 
-# импортируем файл с конфигами
-import settings
-
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
 
 # Создаем функцию для приветствия пользователя
 def greet_user(update, context):
-    # пишем в консоль
-    print('Вызван /start')
-    print(update)
-    # приветствуем пользователя
-    update.message.reply_text(settings.GREETING_MESSAGE)
+    logging.info('Bot has been lauched')
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text('Дороу, жабокрад')
 
 # Создаем функцию для ответа пользователю его же сообщением
 def talk_to_me(update, context):
-    text = update.message.text
-    # пишем в консоль
-    print(text)
-    # ответим пользователю его же сообщением
-    update.message.reply_text(text)
+    user_text = update.message.text
+    logging.info(f'User said {user_text}')
+    print(user_text)
+    update.message.reply_text(user_text)
+
+# Проверяет, что объект есть в списке объектов пакета ephem
+def check_planet(planet):
+    for item in ephem._libastro.builtin_planets():
+        if planet in str(item):
+            return True
+    return False    
+    
+
+def find_constellation(update, context):
+    planet = update.message.text.split()[-1].lower().capitalize()
+    if not check_planet(planet):
+        update.message.reply_text('Сори, жабокрад, такой планеты не видел')
+        return
+    today = date.today()
+    logging.info(f'User asked for a constellation of {planet} for {today}')
+    planet_function = getattr(ephem, planet, None)
+    if planet_function is not None:
+        print(planet_function)
+        planet_data = planet_function(today)
+        print(planet_data)
+        update.message.reply_text(f'Сейчас планета {planet} находится в созвездии {ephem.constellation(planet_data)[-1]}')
+        print(ephem.constellation(planet_data))
+    else:
+        update.message.reply_text('Сори, жабокрад, такой планеты не знаю')
 
 # Тело бота
 def main():
@@ -29,6 +52,7 @@ def main():
     # Объявляем диспетчера и добавляем обработчик для команды /start
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler("planet", find_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Bot has been lauched')
